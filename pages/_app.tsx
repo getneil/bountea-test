60657af154d21a8ddddbfdc4c8f5c5452c3f3bcc
neil paul molina <get.neilmolina@gmail.com>
2022-08-27 16:30:03 +0800

    add loading status when moving to different page using Link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import NextNProgress from 'nextjs-progressbar'
+
 import PageLayout from '../components/PageLayout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PageLayout>
+      <NextNProgress color="#15803c" options={{ showSpinner: false }} />
       <Component {...pageProps} />
     </PageLayout>
   )
