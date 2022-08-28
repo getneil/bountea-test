0b57ae9b81f0f37939c3f9d6fcde174fe83d2d99
neil <neil@neils-MacBook-Pro.local>
2022-08-28 13:57:06 +0800

    test: implement caching

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=7200, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 }
